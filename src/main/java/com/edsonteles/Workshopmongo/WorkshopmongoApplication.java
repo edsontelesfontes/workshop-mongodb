ad82e1fa245284556e6af8b7571aad1551d537c5
José Edson <edson.teles.fontes@gmail.com>
2023-08-08 22:12:17 -0300

    First commit

---
+++ src/main/java/com/edsonteles/Workshopmongo/WorkshopmongoApplication.java
@@ -0,0 +1,13 @@
+package com.edsonteles.Workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopmongoApplication.class, args);
+	}
+
+}
